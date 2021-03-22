66c92563e1370f739463358dd720d817d287b273
Qazkie <79108532+Qazkie@users.noreply.github.com>
2021-03-22 16:16:09 -0400

    Update MotorController.py

--- RPI4/LowLevel/MotorController.py
+++ RPI4/LowLevel/MotorController.py
@@ -5,15 +5,24 @@ import RPi.GPIO as GPIO
 class MotorController():
     def __init__(self, channel1, channel2, channel3):
         self.IN1=DigitalWrite(channel1)
+        self.IN1.set(GPIO.LOW)
         self.IN2=DigitalWrite(channel2)
         self.IN2.set(GPIO.LOW)
         self.ENA=PWMWrite(channel3)
+        self.percentage=0
 
-    def setPercentage(percentage):
+    def setPercentage(self, percentage):
+        self.percentage=percentage
         self.ENA.set(percentage)
 
-    def motorStart():
+    def motorForward(self):
         self.IN1.set(GPIO.HIGH)
+        self.IN2.set(GPIO.LOW)
 
-    def motorStop():
+    def motorBackward(self):
         self.IN1.set(GPIO.LOW)
+        self.IN2.set(GPIO.HIGH)
+
+    def motorStop(self):
+        self.IN1.set(GPIO.LOW)
+        self.IN2.set(GPIO.LOW)
