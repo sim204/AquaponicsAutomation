4d28a40413083b3d644e457514b6be92f59bb313
David <davidlycanada27@gmail.com>
2021-05-16 19:08:33 -0400

    merge error, updated requirement

--- RPI4/CommandStructure/Subsystem/LightLevel.py
+++ RPI4/CommandStructure/Subsystem/LightLevel.py
@@ -3,10 +3,9 @@ import time
 import board
 import busio
 import adafruit_veml7700
-"""
-Code de lecture de Lumière fortement inspiré par et basé sur:
-https://learn.adafruit.com/adafruit-veml7700/python-circuitpython
-"""
+
+#Code de lecture de Lumière fortement inspiré par et basé sur:
+#https://learn.adafruit.com/adafruit-veml7700/python-circuitpython
 class LightLevel(Subsystem.Subsystem):
     
     def __init__(self):
